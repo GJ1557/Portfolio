84dd4a9f465c4d7cb06b1e774f6cd4174f9783ec
GJ1557 <greenjhs1557@gmail.com>
2022-09-07 17:00:43 +0900

    ver2_4

--- zzzzzzzzzzzzzz/test.js
+++ zzzzzzzzzzzzzz/test.js
@@ -1,18 +1,3 @@
-$('body').mouseover(function(){
-    $(this).css({cursor: 'none'});
-  });
-  
-  $('#text1>a').on('mousemove', function(e){
-    $('#cursor').css({
-      left:  e.pageX,
-      top:   e.pageY,
-    });
-  });
-
-  $('#text1>a').on('mouseover', function(e){
-    $('#cursor').addClass('on')
-  });
-
-  $('#text1>a').on('mouseleave', function(e){
-    $('#cursor').removeClass('on')
-  });
+$('.popcorn').on('mouseover', function(e){
+  $(this).addClass('on')
+});
